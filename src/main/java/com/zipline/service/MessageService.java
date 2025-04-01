a6d16fd0e98cf24e4ad8af135267ac024b06f0f9
채지원 <dev.julia.chae@gmail.com>
2025-04-01 15:05:43 +0900

    feat: 메세지 전송 service 코드 추가

---
+++ src/main/java/com/zipline/service/MessageService.java
@@ -0,0 +1,35 @@
+package com.zipline.service;
+
+import com.zipline.dto.SendMessageRequestDto;
+import java.util.Map;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+
+@Service
+public class MessageService {
+
+  private final WebClient webClient;
+
+  public MessageService(WebClient webClient, SignatureService signatureService) {
+    this.webClient = webClient;
+  }
+
+  public Mono<String> sendMessage(SendMessageRequestDto[] request) {
+
+    Map<String, Object> wrappedRequest = Map.of("messages", request);
+
+    return webClient.post()
+        .uri("/send-many/detail")
+        .bodyValue(wrappedRequest)
+        .retrieve()
+        .onStatus(
+            HttpStatusCode::is4xxClientError,
+            clientResponse -> clientResponse.bodyToMono(String.class).map(body -> {
+              throw new RuntimeException(body);
+            })
+        ).bodyToMono(String.class);
+  }
+}
